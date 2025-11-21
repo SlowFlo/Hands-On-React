92fc7d7bf9f3eea94806012632292304e7fd59e0
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-11-21 14:02:37 +0100

    feat: Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,11 +4,23 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-    {rules: {quotes: ["error", "double"]}}
+  eslintPluginPrettier,
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+  { rules: { quotes: ["error", "double"] } },
 ]);
